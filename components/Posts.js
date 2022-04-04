447374207d6a62c0da759041a5bc4a8db8ae4b0c
Samiya <skmcintosh20@gmail.com>
2022-04-03 20:03:09 -0700

    finished the post component

---
+++ components/Posts.js
@@ -0,0 +1,59 @@
+import Post from "./Post";
+
+const realtimePosts = [
+	{
+		name: "Sonny Sangha",
+		image: "https://links.papareact.com/zof",
+		// postImage: "https://links.papareact.com/l4v",
+		message: "Letsssss Go Guyysss!",
+		timestamp: "12-12-21, 10:03pm",
+	},
+	{
+		name: "Elon Musk",
+		image: "https://links.papareact.com/4zn",
+		// postImage: "https://links.papareact.com/kxk",
+		message: "Tesla's is where it's at.",
+		timestamp: " 11-11-21, 7:53pm",
+	},
+	{
+		name: "Jeff Bezoz",
+		image: "https://links.papareact.com/k2j",
+		postImage: "https://links.papareact.com/f0p",
+		message:
+			"I actually went to space, unlike someone else that shall remain nameless.",
+		timestamp: "10-10-21, 6:03pm",
+	},
+	{
+		name: "Mark Zuckerberg",
+		image: "https://links.papareact.com/xql",
+		// postImage: "https://links.papareact.com/snf",
+		message: "I also am not a Scooby Doo villain.",
+		timestamp: "09-09-21, 5:03pm",
+	},
+	{
+		name: "Bill Gates",
+		image: "https://links.papareact.com/4u4",
+		postImage: "https://links.papareact.com/zvy",
+		message: "I am not a Scooby Doo villain!",
+		timestamp: "08-08-21, 04:03pm",
+	},
+];
+
+function Posts() {
+	return (
+		<div>
+			{realtimePosts.map((post) => (
+				<Post
+					key={post.id}
+					name={post.name}
+					message={post.message}
+					timestamp={post.timestamp}
+					image={post.image}
+					postImage={post.postImage}
+				/>
+			))}
+		</div>
+	);
+}
+
+export default Posts;
